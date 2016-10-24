1d7d6b7fdd5107cbf8ec4ee19cbda0dcb1587f3f
Leonardo Turchi <info@spaceclick.com>
2016-10-24 18:10:20 +0200

    Primo programma

--- C#/RS_Engine/RS_Engine/Program.cs
+++ C#/RS_Engine/RS_Engine/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,123 @@ namespace RS_Engine
     {
         static void Main(string[] args)
         {
+
+            //selector
+            Console.WriteLine("\n ** RECOMMENDATORS ENGINE **\n");
+            Console.WriteLine("    1) run");
+            Console.Write("\n");
+
+            //get input
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            //path
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Datasets\");
+
+            //CHOICE: MERGER
+            if (choice == 1)
+            {
+
+                //ui
+                Console.WriteLine(" ");
+                Console.WriteLine("  (long running program) => ready to start ? ");
+                Console.ReadLine();
+
+                Console.WriteLine("  ..processing");
+                Console.WriteLine(" ");
+
+                //read file
+                Console.WriteLine("   ..reading entire archive");
+                var lines = File.ReadAllLines(path + "user_profile" + ".csv");
+                Console.WriteLine("   ..read OK \n\n");
+
+                //PROCESSING
+                float counter = 1;
+                int total = lines.Count() / 2 + 1; //due to \n
+                float progress = 0;
+
+                //SCROLLING
+                foreach (var line in lines)
+                {
+                    //avoiding blank lines
+                    if (line == "") continue;
+
+                    //progress status
+                    progress = (float)(counter * 100 / total);
+                    Console.Write("\r     ..running: line {1} of {2}  |  {0}%", progress.ToString("0.00"), counter, total);
+
+                    //debug
+                    Console.WriteLine(" >>>> LINE = " + line);
+
+                    //converting line
+                    List<int> lineList = line.Split(',').Select(Int32.Parse).ToList();
+
+                    //do stuff with line
+                    //...
+                    //TODO
+
+                    //flusher
+                    lineList.Clear();
+
+                    //cycle counter
+                    counter++;
+
+                    //debug, only first line run
+                    //break;
+                }
+                //line > next
+
+
+                //preparing top matches matrices
+                List<List<object>> valuesMatrix10 = new List<List<object>>();
+
+
+                //OUTPUT
+                //preparing output
+                var otpt =
+                        Environment.NewLine
+                        + " + ELABORATION RESULT + "
+                        + Environment.NewLine
+                        + Environment.NewLine
+                        + " + .... "
+                        + Environment.NewLine
+                        + Environment.NewLine
+                        ;
+
+                otpt += Environment.NewLine;
+                otpt += "--------------------------------";
+                otpt += Environment.NewLine;
+
+                //OUT
+                //console output statistics
+                Console.WriteLine(otpt);
+                Console.WriteLine("\n\n");
+
+                //saving log
+                string outputFileName = @"C:\RS_out\result_" + DateTime.UtcNow.ToString() + ".txt";
+                File.AppendAllText(outputFileName,
+                          "#############################################################"
+                        + Environment.NewLine
+                        + "ANALYSIS OF " + DateTime.UtcNow
+                        + Environment.NewLine
+                        + otpt
+                        + Environment.NewLine
+                        + "#############################################################"
+                        + Environment.NewLine
+                        );
+                Console.WriteLine(" >>>>>> log saved to " + outputFileName + "  :) ");
+
+            }
+
+
+            if (choice == 2)
+            {
+                ;
+            }
+            else
+            {
+                ;
+            }
+
         }
     }
 }
