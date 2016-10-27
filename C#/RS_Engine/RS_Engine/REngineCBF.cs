f28fa0564b1ec8fda7a09cd284739decafa22d08
Leonardo Turchi <info@spaceclick.com>
2016-10-27 13:21:40 +0200

    prima degli 0 e 0 nei coseni

--- C#/RS_Engine/RS_Engine/REngineCBF.cs
+++ C#/RS_Engine/RS_Engine/REngineCBF.cs
@@ -11,39 +11,42 @@ namespace RS_Engine
 
         public static void getRecommendations()
         {
-            //
+            //info
             RManager.outLog("  + processing..");
             RManager.outLog("  + computing CBF.. ");
 
-            /*
-            for (int i = 0; i <= 5; i++)
-                foreach (var p in RManager.user_profile[i])
-                    Console.WriteLine(" ." + i + " ---> " + p);
-            foreach (var p in RManager.user_profile.Last())
-                Console.WriteLine(" .last ---> " + p);
-            */
+            //Creating user-user matrix
 
-            //Creating user-item matrix
+
+            //weights for cosine similarity weighted average
+            double w_cos1 = 0.2;
+            double w_cos2 = 0.5;
+            double w_cos3 = 0.3;
 
             //row
-            int r1 = 0;
-            int r2 = 1;
+            int r1 = 238-2;
+            int r2 = 245-2;
 
-            //cos sim
-            //List<double> cos1a = new List<double>(); //1
-            //List<double> cos1b = new List<double>();
-            double cos1 = 0;
-            double dup_count_cos1 = 0;
+            //cosine similarity
+                //for calculation of: user_profile[row][1]
+                double cos1 = 0;
+                double dup_count_cos1 = 0;
+                //List<double> cos1a = new List<double>();
+                //List<double> cos1b = new List<double>();
+            
+                //for calculation of: user_profile[row][2>>10]
+                double cos2 = 0;
+                List<double> cos2a = new List<double>();
+                List<double> cos2b = new List<double>();
 
-            List<double> cos2a = new List<double>(); //2-10
-            List<double> cos2b = new List<double>();
-            double cos2 = 0;
+                //for calculation of: user_profile[row][11]
+                double cos3 = 0;
+                double dup_count_cos3 = 0;
+                //List<double> cos3a = new List<double>();
+                //List<double> cos3b = new List<double>();
 
-            //List<double> cos3a = new List<double>(); //11
-            //List<double> cos3b = new List<double>();
-            double cos3 = 0;
-            double dup_count_cos3 = 0;
 
+            //call cosine or count calculation for a couple of rows
             for (int i = 0; i <= 11; i++)
             {
                 if (i == 1)
@@ -77,6 +80,7 @@ namespace RS_Engine
                 }
                 if (i>=2 && i<=10)
                 {
+                    //create vectors for cosine computation
                     cos2a.Add(Convert.ToDouble(RManager.user_profile[r1][i]));
                     cos2b.Add(Convert.ToDouble(RManager.user_profile[r2][i]));
                 }
@@ -111,14 +115,16 @@ namespace RS_Engine
                 }
             }
 
-            //double cos1 = GetCosineSimilarity(cos1a, cos1b);
+            //invoke cosine vector similarity computation
             cos2 = GetCosineSimilarity(cos2a, cos2b);
+            //not in use, we are currently checking duplicates:
+            //cos1 = GetCosineSimilarity(cos1a, cos1b);
             //cos3 = GetCosineSimilarity(cos3a, cos3b);
 
             Console.WriteLine();
-            Console.WriteLine("cos1:" + cos1 + "  cos2:" + cos2 + "  cos3:" + cos3);
-            Console.WriteLine(" somma " + cos1 + cos2 + cos3);
-            Console.WriteLine(" media " + (cos1 + cos2 + cos3)/3);
+            Console.WriteLine(" cos1=" + cos1 + " | cos2=" + cos2 + " | cos3=" + cos3);
+            Console.WriteLine(" cos sum= " + cos1 + cos2 + cos3);
+            Console.WriteLine(" weighted avg= " + (cos1*w_cos1 + cos2*w_cos2 + cos3*w_cos3)/3);
             Console.WriteLine();
 
         }
