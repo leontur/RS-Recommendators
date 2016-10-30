df51b0a5077b11f02881405858d6539c2dea6ea9
Leonardo Turchi <info@spaceclick.com>
2016-10-30 15:23:12 +0100

    preparazione lavoro

--- C#/RS_Engine/RS_Engine/REngineEVAL.cs
+++ C#/RS_Engine/RS_Engine/REngineEVAL.cs
@@ -9,10 +9,17 @@ namespace RS_Engine
     class REngineEVAL
     {
 
-        //TRAIN AND TEST SET SPLIT
-        public void splitTrainTestData()
-        {
+        private static double AP;
+        private static double MAP;
+        private static double PREC;
+        private static double RECALL;
+        private static double K;
 
+        //TODO
+        //(sara)
+        public void computePrecision(int user_id, List<int> recommended)
+        {
+            //nota la lista ha count()==5
 
 
         }
---
+++ C#/RS_Engine/RS_Engine/REngineSPLIT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS_Engine
+{
+    class REngineSPLIT
+    {
+
+
+        //TRAIN AND TEST SET SPLIT
+        public void splitTrainTestData()
+        {
+
+
+
+        }
+    }
+}
