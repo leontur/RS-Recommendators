b9448ca477b7cd326d4855bfa587f219513a6121
Leonardo Turchi <info@spaceclick.com>
2016-10-31 18:06:27 +0100

    finito ICF, testing per output

--- C#/RS_Engine/RS_Engine/REngineICF.cs
+++ C#/RS_Engine/RS_Engine/REngineICF.cs
@@ -23,11 +23,19 @@ namespace RS_Engine
      * TODO:
      * -compute pearson correlation coefficient for a couple of users
      * 
-     * -get top 5
+     * -get top SIM_RANGE users similar to target
+     * -get their interactions and merge all to select most commons
+     * -select only not already interacted by target
+     * -select top 5
      * -output
      */
     class REngineICF
     {
+
+        //ALGORITHM PARAMETERS
+        //number of similarities to select (for each item to be recommended)
+        private const int SIM_RANGE = 15;
+
         //MAIN ALGORITHM METHOD
         public static void getRecommendations()
         {
@@ -168,7 +176,7 @@ namespace RS_Engine
                     foreach (var inter in interactions_of_user_top)
                     {
                         int index = interacted.IndexOf(inter);
-                        interactions_titles_of_user_top.Concat(interaction_titles[index]);
+                        interactions_titles_of_user_top = interactions_titles_of_user_top.Concat(interaction_titles[index]).ToList();
                     }
                     //at this point I have the list of all jobs titles for the current user
 
@@ -203,11 +211,13 @@ namespace RS_Engine
             //compute the similarity between two users
             // rows: each target user (count: from target_users)
             // cols: each other user (count: from user_profile)
-            List<double> tgtuser_to_allusers_distance_similarity = new List<double>();
+            List<List<double>> tgtuser_to_allusers_distance_similarity = new List<List<double>>();
 
             //NOTE
-            // the index of the list   all_user_interactions_titles   is the same of the   tgtuser_to_allusers_distance_similarity
-            // beacuse both computed from user_profile
+            // the index of the list   all_user_interactions_titles   
+            //   is the same of   user_profile
+            // the index of the list   tgtuser_to_allusers_distance_similarity   
+            //   is the same of   target_users
 
             //check if already serialized (for fast fetching)
             if (!File.Exists(Path.Combine(RManager.SERIALTPATH, "ICF_tgtuser_to_allusers_distance_similarity.bin")))
@@ -215,6 +225,7 @@ namespace RS_Engine
 
                 //for each user to recommend (u: is the id of the target user)
                 int u1, u2;
+                double sim;
                 for (u1 = 0; u1 < RManager.target_users.Count(); u1++)
                 {
                     //counter
@@ -227,6 +238,9 @@ namespace RS_Engine
                     //user 1 titles
                     List<int> u1T = all_user_interactions_titles[u1ix];
 
+                    //temp sim list
+                    List<double> tmpSim = new List<double>();
+
                     //for each user in dataset
                     for (u2 = 0; u2 < RManager.user_profile.Count(); u2++)
                     {
@@ -234,11 +248,14 @@ namespace RS_Engine
                         List<int> u2T = all_user_interactions_titles[u2];
 
                         //compute similarity between u1 and u2
-                        float sim = computeDistanceBasedSimilarity(u1T, u2T);
+                        sim = computeDistanceBasedSimilarity(u1T, u2T);
 
                         //storing sim
-                        tgtuser_to_allusers_distance_similarity.Add(sim);
+                        tmpSim.Add(sim);
                     }
+
+                    //storing sim vector
+                    tgtuser_to_allusers_distance_similarity.Add(tmpSim);
                 }
 
                 //serialize
@@ -256,7 +273,7 @@ namespace RS_Engine
                 {
                     RManager.outLog("  + reading serialized file " + "ICF_tgtuser_to_allusers_distance_similarity.bin");
                     var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                    tgtuser_to_allusers_distance_similarity = (List<double>)bformatter.Deserialize(stream);
+                    tgtuser_to_allusers_distance_similarity = (List<List<double>>)bformatter.Deserialize(stream);
                 }
             }
 
@@ -265,26 +282,75 @@ namespace RS_Engine
             //info
             RManager.outLog("  + generating output structured data");
 
+            //generating items to recommend for each user
+            List<List<int>> user_user_simil_out = new List<List<int>>();
+
             //for each user to recommend (u: is the id of the target user)
-            foreach (var u in RManager.target_users)
+            //finding recommended items
+            for (int u = 0; u < RManager.target_users.Count; u++)
             {
-                //finding related users
-
                 //recommending top 5 similar items
+                //!! <> da gIT
 
+                //counter
+                if (u % 100 == 0)
+                    RManager.outLog("  - user: {0}" + u, true, true);
 
+                //retrieve the complete list of similarities for the current user
+                List<double> curr_user_line = tgtuser_to_allusers_distance_similarity[u];
 
+                //getting top SIM_RANGE for this user (without considering 1=himself in first position)
+                // transforming the line to a pair (value, index) array
+                // the value is a float, the index a int
+                // the index is used to find the id of the matched user
+                var sorted_curr_user_line = curr_user_line
+                                            .Select((x, i) => new KeyValuePair<double, int>(x, i))
+                                            .OrderByDescending(x => x.Key)
+                                            .Take(SIM_RANGE)
+                                            .ToList();
+                sorted_curr_user_line.RemoveAt(0);
+                //List<float> topforuser = sorted_curr_user_line.Select(x => x.Key).ToList();
+                List<int> useroriginalindex = sorted_curr_user_line.Select(x => x.Value).ToList();
+
+                //retrieving indexes of the users to recommend
+                List<int> similar_users = new List<int>();
+                foreach (var i in useroriginalindex)
+                    similar_users.Add((int)RManager.user_profile[i][0]);
+
+                //retrieving interactions done by each user to recommend (and merging to select most populars)
+                List<int> interactions_of_similar_users = new List<int>();
+                foreach (var i in similar_users)
+                    foreach (var j in RManager.interactions)
+                        if (j[0] == i)
+                            interactions_of_similar_users.Add(j[1]);
+
+                //ADVANCED FILTER
+                //retrieving interactions already used by the current user (not recommendig a job already applied or bookmarked)
+                List<int> already_clicked = RManager.interactions.Where(i => i[0] == u && i[2] == 1).Select(i => i[1]).ToList();
+                //removing already clicked
+                interactions_of_similar_users = interactions_of_similar_users.Except(already_clicked).ToList();
+
+                //selecting most clicked items (top 5)
+                var interactions_of_similar_users_group_by = interactions_of_similar_users
+                                                            .GroupBy(i => i)
+                                                            .OrderByDescending(grp => grp.Count())
+                                                            .Take(5);
+                List<int> interactions_of_similar_users_top = interactions_of_similar_users_group_by.Select(x => x.Key).ToList();
+
+                //saving for output
+                user_user_simil_out.Add(interactions_of_similar_users_top);
             }
         }
 
         //////////////////////////////////////////////////////////////////////////////////////////
 
+        //JOBS TITLES
         //DISTANCE BASED SIMILARITY
-        private static float computeDistanceBasedSimilarity(List<int> titleU1, List<int> titleU2)
+        private static double computeDistanceBasedSimilarity(List<int> titleU1, List<int> titleU2)
         {
             //check if no one in common
             if (titleU1.Intersect(titleU2).Count() == 0)
-                return 0;
+                return (double)0;
 
             //compute the distance
             double num = 1, den = 0, squares_sum = 0;
@@ -294,7 +360,7 @@ namespace RS_Engine
                         squares_sum += Math.Pow(titleU1[i1] - titleU2[i2], 2);
 
             den = 1 + squares_sum;
-            return (float)(num / den);
+            return (double)(num / den);
         }
 
     }
